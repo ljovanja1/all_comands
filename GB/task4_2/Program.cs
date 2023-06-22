a4583d864ca3bc97c7600209a7ca1853c2ed3896
roman <you@example.com>
2023-06-23 00:01:03 +0300

    add task4_2

---
+++ GB/task4_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace task2 {
+
+    class Program {
+
+        static void Main() {
+        
+            Console.Write("Введите первое число: ");
+            int first = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            int second = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            int third = Convert.ToInt32(Console.ReadLine());
+            int max = 0;
+            if (first > second) {
+                max = first;
+            }
+                if (second > first) {
+                    max = second;
+                }
+                    if (third > second) {
+                        max = third;
+                    }
+            else {
+                Console.WriteLine("DELETE");
+            }
+            Console.WriteLine(max);
+        }
+
+    }
+
+}
