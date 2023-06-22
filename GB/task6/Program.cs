4175a5bd1fa4c282c5a36b38526ce27e7937e637
roman <you@example.com>
2023-06-23 00:18:30 +0300

    task8

--- GB/task6/Program.cs
+++ GB/task6/Program.cs
@@ -3,3 +3,27 @@
 // -3 -> нет
 // 7 -> нет
 
+using System;
+
+namespace task2 {
+
+    class Program {
+
+        static void Main() {
+        
+            Console.Write("Введите первое число: ");
+            int first = Convert.ToInt32(Console.ReadLine());
+
+            if ((first % 2) == 0) {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            
+            }
+        }
+
+    }
+
+}
